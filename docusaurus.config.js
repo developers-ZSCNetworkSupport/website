d28bd683cde5804d25af0ac2aefb3aa32f8bef5b
govolokatliai <govolokatliai@gmail.com>
2024-06-27 19:41:26 +0800

    add add add

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -46,14 +46,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/developers-ZSCNetworkSupport/website',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/developers-ZSCNetworkSupport/website',
         },
         theme: {
           customCss: './src/css/custom.css',
